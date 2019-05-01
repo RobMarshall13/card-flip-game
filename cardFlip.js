b0d5b17f671cd7ed807e5acd32dff7d1dc3c8dbe
robmarshall13 <codemelt@outlook.com>
2019-05-01 12:04:47 +0100

    checkSolvable if statement does not execute

--- cardFlip.js
+++ cardFlip.js
@@ -1,40 +1,53 @@
-var array=[1,1,1,1,1,1,1]
-console.log(array);
-var modifiedArray = [];
-var wasOne=[];
+var array = [1,0,1,1,0,0,0]
+console.log('original array ' + array)
 
-
-function CheckingForOne (){
-   
-        var i;
-        for (i = 0; i < array.length; i++) {
-        // if there is a one  save index to variable(wasOne) and flipEitherSide()
-        if (array[i] == 1){
-            wasOne = array.indexOf(i);
-            array[wasOne] = '.';
-            console.log(array[wasOne]);
-            // flipEitherSide(wasOne);
+function checkSolvable(){
+    for( i =0; i < array.length; i++){
+        if ((array[i] == '.') && ((i > 0) && (i < array.length))) {
+           
+            lowerArray = array.slice(0,i);
+            console.log('lower array ' + lowerArray);
+            upperArray = array.slice(i, array.length)
+            console.log('upper array ' + upperArray)
+            if(upperArray.includes(1)){
+                if( lowerArray.includes(1)){
+                findOne();
+            }else{
+                console.log('unsolvable')
+            }
         }else{
-            console.log(" no ones" + array);
+            console.log('unsolvable')
         }
-        
-      };
-//if there are no 1's check for 0, if 0 is true alert cant be solved
-// if no ones and no zero's alert solved
-
+    }findOne();
+}
 }
 
-function flipEitherSide(wasOne){
-    let oneAbove = wasOne += 1;//index of one above
-    let oneBelow = wasOne -= 1;// index of one below
-    //change array[wasOne] to '.' and flipEitherSide()
-   
-    // if
-    // array[oneAbove] = 1 ? 0 : 1;
-    // array[oneBelow] = 1 ? 0 : 1;
-  
-    // CheckingForOne();
+function findOne(){
+    for ( i = 0; i < array.length; i++){
+        if ((array[i]) == 1){
+         array[i] = '.';
+         if((i-1 >= 0)){
+         if ((array[i-1] != 1) && (array[i-1] != '.') ){
+             array[i-1] = 1;
+         }else if((array[i-1] != 0) && (array[i-1] != '.') ){
+                array[-1] = 0;
+         }else{};
+        }
+        if((i+1 < array.length)){
+            if ((array[i+1] != 1) && (array[i+1] != '.') ){
+                array[i+1] = 1;
+            }else if((array[i+1] != 0) && (array[i+1] != '.') ){
+                   array[+1] = 0;
+            }else{};
+           }
+           checkSolvable();
+         console.log(array);
+        }
+    }
     
-//charAt wasOne +=1/-=1 change to 1?0:1
 }
-CheckingForOne();
+
+
+
+// findOne();
+checkSolvable();
